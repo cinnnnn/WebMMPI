73e2d45cbb70bc3012add5aea5039d59a74fdd17
erickavila151 <erickavila414@gmail.com>
2020-08-22 00:37:11 -0500

    #9 - added email section

--- index.php
+++ index.php
@@ -887,107 +887,50 @@
 		<!-- Footer start -->
 		<footer id="footer" class="footer">
 			<div class="container">
-				<div class="row">
-				<div class="col-md-4 col-sm-12 footer-widget">
-					<h3 class="widget-title">Recent Posts</h3>
-					<div class="latest-post-items media">
-					<div class="latest-post-content media-body">
-						<h4><a href="#">Bulgaria claims to find Europe's 'oldest town'</a></h4>
-						<p class="post-meta">
-						<span class="author">Posted by John Doe</span>
-						<span class="post-meta-cat">in<a href="#"> Blog</a></span>
-						</p>
-					</div>
-					</div><!-- 1st Latest Post end -->
-
-					<div class="latest-post-items media">
-					<div class="latest-post-content media-body">
-						<h4><a href="#">Few Answers in Case of Murdered Law Professor</a></h4>
-						<p class="post-meta">
-						<span class="date"><i class="icon icon-calendar"></i> Mar 15, 2015</span>
-						<span class="post-meta-comments"><i class="icon icon-bubbles4"></i> <a href="#">03</a></span>
-						</p>
-					</div>
-					</div><!-- 2nd Latest Post end -->
-
-					<div class="latest-post-items media">
-					<div class="latest-post-content media-body">
-						<h4><a href="#">Over the year we have lots of experience in our field</a></h4>
-						<p class="post-meta">
-						<span class="date"><i class="icon icon-calendar"></i> Apr 17, 2015</span>
-						<span class="post-meta-comments"><i class="icon icon-bubbles4"></i> <a href="#">14</a></span>
-						</p>
-					</div>
-					</div><!-- 3rd Latest Post end -->
-
-				</div>
-				<!--/ End Recent Posts-->
-
-
-				<div class="col-md-4 col-sm-12 footer-widget">
-					<h3 class="widget-title">Flickr Photos</h3>
-
-					<div class="img-gallery">
-					<div class="img-container">
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/1.jpg">
-						<img src="images/gallery/1.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/2.jpg">
-						<img src="images/gallery/2.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/3.jpg">
-						<img src="images/gallery/3.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/4.jpg">
-						<img src="images/gallery/4.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/5.jpg">
-						<img src="images/gallery/5.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/6.jpg">
-						<img src="images/gallery/6.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/6.jpg">
-						<img src="images/gallery/7.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/6.jpg">
-						<img src="images/gallery/8.jpg" alt="">
-						</a>
-						<a class="thumb-holder" data-rel="prettyPhoto" href="images/gallery/6.jpg">
-						<img src="images/gallery/9.jpg" alt="">
-						</a>
-					</div>
-					</div>
-				</div>
-				<!--/ end flickr -->
-
-				<div class="col-md-3 col-sm-12 footer-widget footer-about-us">
-					<h3 class="widget-title">About Craft</h3>
-					<p>We are a awward winning multinational company. We believe in quality and standard worldwide.</p>
-					<h4>Address</h4>
-					<p>1102 Saint Marys, Junction City, KS</p>
+			<div class="row">
+			<div class="col-md-7">
+				<form id="contact-form" action="contact-form.php" method="post" role="form">
 					<div class="row">
-					<div class="col-md-6">
-						<h4>Email:</h4>
-						<p>info@craftbd.com</p>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label>Nombre completo</label>
+								<input class="form-control" name="name" placeholder="" type="text" required>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label>Número de contacto</label>
+								<input class="form-control" name="phone" placeholder="" type="email" required>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group">
+								<label>Correo electrónico</label>
+								<input class="form-control" name="email" placeholder="" required>
+							</div>
+						</div>
 					</div>
-					<div class="col-md-6">
-						<h4>Phone No.</h4>
-						<p>+(785) 238-4131</p>
+					<div class="form-group">
+						<label>¿En qué te podemos ayudar?</label>
+						<textarea class="form-control" name="message"  placeholder="" rows="10" required></textarea>
 					</div>
+					<div class="text-right"><br>
+						<button class="btn btn-primary solid blank" name="submit" type="submit">Enviar mensaje</button>
 					</div>
-					<form action="#" role="form">
-					<div class="input-group subscribe">
-						<input type="email" class="form-control" placeholder="Email Address" required="">
-						<span class="input-group-addon">
-						<button class="btn" type="submit"><i class="fa fa-envelope-o"> </i></button>
-						</span>
-					</div>
-					</form>
+				</form>
+			</div>
+			<div class="col-md-5">
+				<div class="contact-info ">
+					<h3 style="color:#3C8FD3;">CONTÁCTENOS</h3>
+					<p>Estamos a tus ordenes.</p>
+					<br>
+					<p><i class="fa fa-home info"></i> Calz. José Guadalupe Gallo 8592, El Campanario, 45609 Zapopan, Jal., Mexico </p>
+					<!--<p><i class="fa fa-phone info"></i> +(785) 238-4131 </p>
+					<p><i class="fa fa-envelope-o info"></i> info@bizcraft.com</p>
+					<p><i class="fa fa-globe info"></i> www.bizcraft.com</p>-->
 				</div>
-				<!--/ end about us -->
-
-				</div><!-- Row end -->
+			</div>
+		</div>
 			</div><!-- Container end -->
 		</footer><!-- Footer end -->
 
